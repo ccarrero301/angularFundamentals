f813471abdd61d612c34b84c189d60e6112ee3a6
Carlos Carrero <carlos.carrero@gmail.com>
2020-04-28 22:52:27 -0500

    Bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  title: string;
+  isHappy: boolean = false;
+  logo: string = 'img/Geralt.jpg'
+  name: string = 'Todd'
+  numberOne: number = 1;
+  numberTwo: number = 2;
 
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
+
+  handleBlur(event: any) {
+    this.name = event.target.value;
+  }
+
+  handleInput(event: any) {
+    this.name = event.target.value;
+    console.log('handleInput');
+  }
+
+  handleClick() {
+    this.name = 'MOTTO!';
+  }
+
+  handleChange(value: string) {
+    this.name = value;
+    console.log('handleChange');
+  }
+
+  getValue(value: string) {
+    console.log(value);
+  }
 }
