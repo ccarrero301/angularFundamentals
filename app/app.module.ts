431ee6aeceef905ee23dd91759fc8f8f6472f789
Carlos Carrero <carlos.carrero@gmail.com>
2020-05-01 00:07:06 -0500

    very basic routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,19 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found.component';
+import { HomeComponent } from './home.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     // angular modules
     BrowserModule,
     CommonModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     // custom modules
     PassengerDashboardModule
   ],
